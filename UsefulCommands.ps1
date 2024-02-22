94aef6d0fb97fc8f4b016128279041c6b13af1a3
Suriyawong <dunbar.m@gmail.com>
2024-02-22 00:33:47 -0700

    Added command to pull local admins from all member servers.

--- UsefulCommands.ps1
+++ UsefulCommands.ps1
@@ -17,3 +17,10 @@ Get-Date -UFormat "%m-%d-%y"
 ### Certificate stuff
 # Request a computer certificate from an internal PKI. Note that the certificate template has to be available and you must have the 'Enroll' permission for that template.
 Get-Certificate -SubjectName "CN=www.example.com" -Template "YourTemplateName" -DnsName "www.example.com" -CertStoreLocation Cert:\LocalMachine\My
+
+
+### Server Management
+# Retrieve Local Administrators from all Member Servers and output to a CSV file
+Invoke-Command (Get-ADComputer -Filter {OperatingSystem -like "*server*" -and Enabled -eq $true} | Select-Object -ExpandProperty -DnsName) -Credential (Get-Credential -Message "Provide an account with admin credentials to all member servers") -ScriptBlock {
+    Get-LocalGroupMember -SID S-1-5-32-544 | Select-Object *
+} | Export-Csv -Path .\LocalAdmins.csv -NoTypeInformation
